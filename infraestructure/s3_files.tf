e51f2fbd1d2b8a072cd5aca06e5bd6ff6e9cf4e4
PauloGuerreiroo <pauloguerreirobruno@outlook.com>
2021-08-15 10:41:19 -0300

    alteração caminho jobSpark

--- infraestructure/s3_files.tf
+++ infraestructure/s3_files.tf
@@ -3,5 +3,5 @@ resource "aws_s3_bucket_object" "codigo_spark" {
   key    = "emr-code/pyspark/job_spark_from_tf.py"
   acl    = "private"
   source = "../job_spark.py"
-  etag = filemd5("../infraestructure/job_spark.py")
+  etag = filemd5("../job_spark.py")
 }
