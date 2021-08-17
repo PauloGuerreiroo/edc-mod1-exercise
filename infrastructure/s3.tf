f3207889820b828df31c78e62dc7ac0ed8386703
PauloGuerreiroo <pauloguerreirobruno@outlook.com>
2021-08-17 20:37:26 -0300

    Comentando processo streaming

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -16,21 +16,21 @@ resource "aws_s3_bucket" "dl" {
       }
     }
   
-}
-resource "aws_s3_bucket" "stream" {
-  bucket = "igti-guerreiro-streaming-bucket"
-  acl    = "private"
+# }
+# resource "aws_s3_bucket" "stream" {
+#   bucket = "igti-guerreiro-streaming-bucket"
+#   acl    = "private"
 
-  tags = {
-    IES   = "IGTI",
-    CURSO = "EDC"
-  }
+#   tags = {
+#     IES   = "IGTI",
+#     CURSO = "EDC"
+#   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
 }
